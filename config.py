a9361899e55e1ffa195bef4caecb3b98ff89103a
Petrol <Petrol.davies9@outlook.com>
2023-11-03 21:38:38 +0000

    2:08:39

---
+++ config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+def get_config():
+    return {
+        'batch_size': 8, #number of sentences to process in one go
+        'num_epochs': 20, #number of times to go through the dataset
+        'lr': 10**-4,   #learning rate
+        'seq_len': 128, #maximum length of a sentence
+        'lang_src': 'cy', #language pair
+        'lang_trg': 'en', #language pair
+        'model_folder': 'weights', #path to save the model
+        'model_name': 'tmodel_', #name of the model
+        'preloaded_model': None, #path to a preloaded model
+        'tokenizer_path': 'tokenizer_{0}.json', #path to the tokenizer
+        'experiment_name': 'experiment', #name of the experiment
+    }
+
+def gwt_weights_file_name(config,_epoch: str):
+    model_folder = config['model_folder']
+    model_name = config['model_name']
+    model_filename = f"{experiment_name}{epoch}.pt"
+    return os.path.join(model_folder, model_filename)
--- train.py
+++ train.py
@@ -2,12 +2,19 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset, random_split
 
+from dataset import EnglishToWelshDataset, casual_mask
+from Model import build_transformer
+
+from config import get_config, get_weights_file_name
+
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
+
+from torch.utils.tensorboard import SummaryWriter # For logging to Tensorboard 
 
 from pathlib import Path # For file paths handling
 
@@ -43,6 +50,59 @@ def get_ds(config):
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = torch.utils.data.random_split(ds_raw, [train_ds_size, val_ds_size])
 
+    train_ds = EnglishToWelshDataset(train_ds_raw, Tokenizer_src, Tokenizer_trg, config['en'], config['cy'], config['seq_len'])
+    val_ds = EnglishToWelshDataset(val_ds_raw, Tokenizer_src, Tokenizer_trg, config['en'], config['cy'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_trg = 0
+
+    for item in ds_raw:
+        src_ids = Tokenizer_src.encode(item['translation']['en']).ids
+        trg_ids = Tokenizer_src.encode(item['translation']['cy']).ids
+        max_len_src = max(max_len_src, len(src))
+        max_len_trg = max(max_len_trg, len(trg))
+
+    print(f'Maximum length of source sentence: {max_len_src}')
+    print(f'Maximum length of target sentence: {max_len_trg}')
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, Tokenizer_src, Tokenizer_trg
 
 
+    def get_model(config, vocab_src_len, vocab_trg_len):
+        model = nn.Transformer(vocab_src_len, vocab_trg_len, config['seq_len'], config['seq_len'], config['d_model'])
+        return model
+
+
+def train_model(config):
+# definw the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using device: {device}')
+
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, Tokenizer_src, Tokenizer_trg = get_ds(config)
+    model = get_model(config, len(Tokenizer_src.get_vocab()), len(Tokenizer_trg.get_vocab().to(device)))
+
+    # Tensorboard
+    writer = SummaryWriter(f'runs/{config["experiment_name"]}')
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+    if config['preloaded_model']:
+        model_filename = get_weights_file_name(config, config['preloaded_model'])
+        print(f'Loading model from {model_filename}')
+        state -= torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer'])
+
+
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=Tokenizer_src.token_to_id(['[PAD]']), label_smoothing=0.1).to(device) #ignore the padding token, label smoothing is a regularization technique to prevent overfitting
+
+    for epoch in range(initial_epoch, config['num_epochs']):
 
